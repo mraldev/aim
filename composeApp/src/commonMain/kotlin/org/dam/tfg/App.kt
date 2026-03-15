744da78bf3eef8bd5eee6995d447297ef9b65bd4
AntoVD <antoniovaquerada@gmail.com>
2026-03-15 18:41:46 +0100

    Introducido campo contraseña + verificación de datos introducidos

--- composeApp/src/commonMain/kotlin/org/dam/tfg/App.kt
+++ composeApp/src/commonMain/kotlin/org/dam/tfg/App.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.TextField
@@ -26,29 +25,45 @@ fun App() {
 class MainScreen : Screen {
     @Composable
     override fun Content() {
+
         var usuario: String by remember { mutableStateOf("") }
         var frase: String by remember { mutableStateOf("") }
+        var contrasenya : String by remember { mutableStateOf("") }
+
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally,) {
             Spacer(modifier = Modifier.height(38.dp))
             Text("Usuario")
             TextField(value = usuario, onValueChange = { usuario = it })
+            Text("Contraseña")
+            TextField(value = contrasenya, onValueChange = { contrasenya = it })
             Text(frase)
-            Button(onClick = {
-                if (usuario.isNotEmpty()) {
-                    var valido = verificarUsuario(usuario);
-                    if (valido) {
-                        frase = "El usuario es: $usuario"
+            Button(  onClick = {
+                if (usuario.isNotEmpty() && contrasenya.isNotEmpty()) {
+                    var usuarioValido = (verificarUsuario(usuario) && verificarContrasenya(contrasenya))
+                    if (usuarioValido) {
+                        frase = "Bienvenido a AIM: $usuario"
+                    }
+                    else {
+                        frase = "Datos introducidos no validos."
                     }
                     } else {
-                        frase = ""
+                        frase = "Faltan datos a introducir."
                     }
             }) {
+                Text("LOGIN")
             }
         }
     }
 
     fun verificarUsuario(usuario:String): Boolean {
         if (usuario == "admin") {
+            return true
+        }
+        return false
+    }
+
+    fun verificarContrasenya(contrasenya:String): Boolean {
+        if (contrasenya == "admin") {
             return true
         }
         return false
