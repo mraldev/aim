3b72c4970bf477c8974c65c4b81f6260c5d904db
AntoVD <antoniovaquerada@gmail.com>
2026-04-09 20:08:09 +0200

    Pantalla vacia como esqueleto a copiar + pantalla ajustes + elementos mejor diseñados y comentados

--- composeApp/src/commonMain/kotlin/org/dam/tfg/screens/Settings.kt
+++ composeApp/src/commonMain/kotlin/org/dam/tfg/screens/Settings.kt
@@ -1,6 +1,5 @@
 package org.dam.tfg.screens
 
-
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,7 +9,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,34 +22,30 @@ import org.dam.tfg.customElements.buttonBar
 import org.dam.tfg.customElements.profileBar
 import org.dam.tfg.model.Users
 
-class Home(private val usuario: Users): Screen {
-    //? En esta ventana no vamos a usar isAdmin, pero de aqui se la podemos pasar a todas las demas ventanas
-    //? lo que significa que podemos saber cuando un usuario es admin en cualquier punto de la app
-
+class Settings(private val usuario: Users): Screen {
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
         Box(modifier = Modifier.fillMaxSize()) {
-            Column(modifier = Modifier.verticalScroll(rememberScrollState()).padding(18.dp).fillMaxSize()){
-                profileBar( usuario, navigator)
-
-
+            Column(modifier = Modifier.verticalScroll(rememberScrollState()).padding(18.dp).fillMaxSize()) {
+            profileBar( usuario, navigator)
             Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = "Bio",
-                style = MaterialTheme.typography.titleMedium
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            repeat(100) {
-                Text(
-                    text = usuario.descripcion //? Biografia del usuario
-                    //* Si hay mas texto que el de la pantalla, este se deberia de hacer scrolleable, por el verticalScroll
-                )
+
+                //TODO : AÑADIR FUNCIONALIDAD DE LOS DIFERENTES BOTONES
+                Row{
+                    Button(onClick = { navigator.push(Login()) }) { Text("Cambiar nombre de usuario") }
+                }
+                Row{
+                    Button(onClick = { navigator.push(Login()) }) { Text("Cambiar Contraseña") }
+                }
+                Row{
+                    Button(onClick = { navigator.push(Login()) }) { Text("Cerrar Sesión") }
+                }
             }
-        }
+
             Row(modifier = Modifier.align(alignment = Alignment.BottomCenter)) {
                 buttonBar(Modifier, navigator)
             }
+        }
     }
-}
 }
