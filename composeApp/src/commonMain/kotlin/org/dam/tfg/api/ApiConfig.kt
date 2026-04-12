9c0c5a92927c8ba8021a1e84839d4581d67dfbb7
Marcos Alonso <mr.marcosalonso.p@gmail.com>
2026-04-12 20:30:49 +0200

    Versión Release. Ip apuntando al ordenador de iván

--- composeApp/src/commonMain/kotlin/org/dam/tfg/api/ApiConfig.kt
+++ composeApp/src/commonMain/kotlin/org/dam/tfg/api/ApiConfig.kt
@@ -5,6 +5,6 @@ object ApiConfig {
     //* Esta es la api que usaremos en produccion
 //    const val BASE_URL = "http://137.101.88.89:6767/api"
     //?  Cambiar a la api necesaria (10.0.2.2 es una dirección especial que apunta a tu máquina anfitrión)
-    const val BASE_URL = "http://10.0.2.2:6767/api"
+    const val BASE_URL = "http://137.101.88.89:6767/api"
 
 }
