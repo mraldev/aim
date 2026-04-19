ebc6367241da4810eb52d8028db2ed70d4d77486
AntoVD <antoniovaquerada@gmail.com>
2026-04-19 17:50:52 +0200

    Botones de confirmar email / contraseña / darse de baja con funcionalidad terminado.

--- composeApp/src/commonMain/kotlin/org/dam/tfg/screens/Settings.kt
+++ composeApp/src/commonMain/kotlin/org/dam/tfg/screens/Settings.kt
@@ -13,10 +13,13 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,16 +32,23 @@ import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import io.ktor.client.request.post
+import kotlinx.coroutines.launch
 import org.dam.tfg.api.ApiClient
 import org.dam.tfg.api.ApiConfig
 import org.dam.tfg.api.authorization.TokenManager
 import org.dam.tfg.api.managers.SesionManager
 import org.dam.tfg.api.managers.UserManager
+import org.dam.tfg.customElements.DialogBase
+import org.dam.tfg.customElements.Tirada.TiradaDialog
 import org.dam.tfg.customElements.buttonBar
 import org.dam.tfg.customElements.profileBar
+import org.dam.tfg.dto.UsuarioTiradaDTO
+import org.dam.tfg.enums.TipoCircuito
+import org.dam.tfg.model.Tirada.Sesion
+import org.dam.tfg.model.Tirada.Tirada
 
 class Settings() : Screen {
-    private fun limpiarManagers(){
+    private fun limpiarManagers() {
         SesionManager.clear()
         UserManager.clear()
         TokenManager.clear()
@@ -49,11 +59,127 @@ class Settings() : Screen {
         var contrasenyaNueva: String by remember { mutableStateOf("") }
         var correoNuevo: String by remember { mutableStateOf("") }
 
+        val regex = Regex(
+            pattern = "^[A-Za-z0-9+._%\\-]{1,64}@[A-Za-z0-9\\-]+(\\.[A-Za-z0-9\\-]+)*\\.[A-Za-z]{2,}$",
+            option = RegexOption.IGNORE_CASE
+        )
+
         val navigator = LocalNavigator.currentOrThrow
+
+        var showEmailDialog by remember { mutableStateOf(false) }
+        var showPasswordDialog by remember { mutableStateOf(false) }
+        var showConfirmationDialog by remember { mutableStateOf(false) }
+
+        if (showConfirmationDialog) {
+            DialogBase(
+                data = mapOf(
+                    "header" to "Dar de baja",
+                    "content" to "¿Seguro que quieres dar de baja tu cuenta?",
+                    "confirmButton" to "Confirmar",
+                    "dismissButton" to "Cancelar"
+                ),
+                onConfirm = {
+                    suspend {
+                        ApiClient.client.post(
+                            "${ApiConfig.BASE_URL}/baja/" + UserManager.correo.value
+                        )
+                    }
+                    limpiarManagers()
+                    navigator.push(Home());
+                },
+                onDismiss = { showConfirmationDialog = false }
+            )
+        }
+
+        if (showEmailDialog) {
+            AlertDialog(
+                onDismissRequest = { showEmailDialog = false },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            if (correoNuevo.isNullOrEmpty() || !regex.matches(correoNuevo)) {
+                                correoNuevo = "Correo no valido."
+                            } else {
+                                UserManager.setCorreo(correoNuevo)
+                                showEmailDialog = false
+                            }
+                        }
+                    ) {
+                        Text("Confirmar")
+                    }
+                },
+                dismissButton =
+                    {
+                        TextButton(
+                            onClick = { showEmailDialog = false }
+                        ) {
+                            Text("Cancelar")
+                        }
+                    },
+                title = { Text("Cambiar Email") },
+                text = {
+                    OutlinedTextField(
+                        value = correoNuevo,
+                        onValueChange = { correoNuevo = it },
+                        label = { Text("Nuevo Email") },
+                        singleLine = true,
+                        shape = RoundedCornerShape(40.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 30.dp)
+                    )
+                },
+                shape = MaterialTheme.shapes.large,
+            )
+        }
+        if (showPasswordDialog) {
+            AlertDialog(
+                onDismissRequest = { showPasswordDialog = false },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            if (correoNuevo.isNullOrEmpty()) {
+                                contrasenyaNueva = "Correo no valido."
+                            } else {
+                                UserManager.setCorreo(correoNuevo)
+                                showPasswordDialog = false
+                            }
+                        }
+                    ) {
+                        Text("Confirmar")
+                    }
+                },
+                dismissButton =
+                    {
+                        TextButton(
+                            onClick = { showPasswordDialog = false }
+                        ) {
+                            Text("Cancelar")
+                        }
+                    },
+                title = { Text("Cambiar Contraseña") },
+                text = {
+                    OutlinedTextField(
+                        value = contrasenyaNueva,
+                        onValueChange = { contrasenyaNueva = it },
+                        label = { Text("Nueva Contraseña") },
+                        singleLine = true,
+                        shape = RoundedCornerShape(40.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 30.dp)
+                    )
+                },
+                shape = MaterialTheme.shapes.large,
+            )
+        }
+
+
         Box(modifier = Modifier.fillMaxSize()) {
             Column(
                 modifier = Modifier.verticalScroll(rememberScrollState()).padding(18.dp)
-                    .fillMaxSize().recalculateWindowInsets(), horizontalAlignment = Alignment.CenterHorizontally,
+                    .fillMaxSize().recalculateWindowInsets(),
+                horizontalAlignment = Alignment.Start,
             ) {
 //.*[@].*[.].* TOMA PUTO REGEX
                 //El regex ya ha sido implementado en LogIn, por favor que se use el mismo formato
@@ -66,41 +192,19 @@ class Settings() : Screen {
                 //De momento se guarda tan solo en el manager local hasta que pueda guardar de forma completamente
                 //segura mediante la API.
                 //TODO : AÑADIR FUNCIONALIDAD DE LOS DIFERENTES BOTONES
-                Row {
-                    OutlinedTextField(
-                        value = contrasenyaNueva,
-                        onValueChange = { contrasenyaNueva = it },
-                        label = { Text("Nueva Contraseña") },
-                        singleLine = true,
-                        shape = RoundedCornerShape(40.dp),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 30.dp)
-                    )
-                }
+
 
                 Row {
                     Button(
-                        onClick = { UserManager.setContrasenya(contrasenyaNueva) },
+                        onClick = { showPasswordDialog = true },
                         modifier = Modifier.width(200.dp)
                     ) { Text("Cambiar contraseña") }
-                }
-                Row {
-                    OutlinedTextField(
-                        value = correoNuevo,
-                        onValueChange = { correoNuevo = it },
-                        label = { Text("Nuevo Correo") },
-                        singleLine = true,
-                        shape = RoundedCornerShape(40.dp),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 30.dp)
-                    )
+
                 }
                 //TODO : Funcionalidad para confirmación de cambiar de correo de forma segura.
                 Row {
                     Button(
-                        onClick = { UserManager.setCorreo(correoNuevo) },
+                        onClick = { showEmailDialog = true },
                         modifier = Modifier.width(200.dp)
                     ) { Text("Cambiar Correo") }
                 }
@@ -120,11 +224,8 @@ class Settings() : Screen {
                 //por el amor de dios no la utiliceis sin cuentas mock solo para probarlo
                 Row {
                     Button(onClick = {
-                        suspend {
-                            ApiClient.client.post(
-                                "${ApiConfig.BASE_URL}/baja/" + UserManager.correo.value
-                            )
-                        }
+                        showConfirmationDialog = true;
+
                     }, modifier = Modifier.width(200.dp)) { Text("Dar la cuenta de baja") }
                 }
             }
@@ -134,3 +235,4 @@ class Settings() : Screen {
         }
     }
 }
+
