5a71ce93eb8a634d75a9142b57aa513de3dd5486
Marcos Alonso <mr.marcosalonso.p@gmail.com>
2026-05-15 22:28:36 +0200

    Button bar

--- composeApp/src/commonMain/kotlin/org/dam/tfg/screens/Historial/HistorialScreen.kt
+++ composeApp/src/commonMain/kotlin/org/dam/tfg/screens/Historial/HistorialScreen.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
@@ -64,11 +66,16 @@ class HistorialScreen(val sesiones: List<SesionHistorial> = emptyList()) : Scree
                 }
             } else {
                 if (sesiones.isEmpty()) {
-                    Box(
-                        modifier = Modifier.fillMaxSize(),
-                        contentAlignment = Alignment.Center
+                    Scaffold(
+                        containerColor = Color.Transparent,
+                        bottomBar = { buttonBar() }
                     ) {
-                        Text("Aún no tienes sesiones registradas")
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text("Aún no tienes sesiones registradas")
+                        }
                     }
                 } else {
                     HistorialContent(sesiones)
